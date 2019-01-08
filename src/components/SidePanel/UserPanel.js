7994c73b7f1dcd903d866fe9cb3a3cdaa6034e04
Scott Schottler <scottschottler@gmail.com>
2019-01-08 17:29:52 -0500

    added avatar editor

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import { Grid, Header, Icon, Image, Dropdown } from 'semantic-ui-react'
+import {
+  Grid,
+  Header,
+  Icon,
+  Image,
+  Dropdown,
+  Modal,
+  Input,
+  Button
+} from 'semantic-ui-react'
+import AvatarEditor from 'react-avatar-editor'
 import firebase from '../../firebase'
 
 export class UserPanel extends Component {
   state = {
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    modal: false,
+    previewImage: '',
+    croppedImage: '',
+    blob: ''
   }
+
+  openModal = () => this.setState({ modal: true })
+  closeModal = () => this.setState({ modal: false })
 
   dropdownOptions = () => [
     {
@@ -19,13 +36,25 @@ export class UserPanel extends Component {
     },
     {
       key: 'avatar',
-      text: <span>Change Avatar</span>
+      text: <span onClick={this.openModal}>Change Avatar</span>
     },
     {
       key: 'signout',
       text: <span onClick={this.handleSignout}>Sign Out</span>
     }
   ]
+
+  handleChange = event => {
+    const file = event.target.files[0]
+    const reader = new FileReader()
+
+    if (file) {
+      reader.readAsDataURL(file)
+      reader.addEventListener('load', () => {
+        this.setState({ previewImage: reader.result })
+      })
+    }
+  }
 
   handleSignout = () => {
     firebase
@@ -34,8 +63,17 @@ export class UserPanel extends Component {
       .then(() => console.log('signed out'))
   }
 
+  handleCropImage = () => {
+    if (this.avatarEditor) {
+      this.avatarEditor.getImageScaledToCanvas().toBlob(blob => {
+        let imageUrl = URL.createObjectURL(blob)
+        this.setState({ croppedImage: imageUrl, blob })
+      })
+    }
+  }
+
   render() {
-    const { user } = this.state
+    const { user, modal, previewImage, croppedImage } = this.state
     const { primaryColor } = this.props
 
     return (
@@ -59,6 +97,59 @@ export class UserPanel extends Component {
             </Header>
           </Grid.Row>
         </Grid.Column>
+
+        <Modal open={modal} onClose={this.closeModal}>
+          <Modal.Header>Change Avatar</Modal.Header>
+          <Modal.Content>
+            <Input
+              fluid
+              onChange={this.handleChange}
+              type="file"
+              label="New Avatar"
+              name="previewImage"
+            />
+
+            <Grid centered stackable columns={2}>
+              <Grid.Row centered>
+                <Grid.Column className="ui center aligned grid">
+                  {previewImage && (
+                    <AvatarEditor
+                      ref={node => (this.avatarEditor = node)}
+                      image={previewImage}
+                      width={120}
+                      height={120}
+                      border={50}
+                      scale={1.2}
+                    />
+                  )}
+                </Grid.Column>
+                <Grid.Column>
+                  {croppedImage && (
+                    <Image
+                      style={{ margin: '3.5em auto' }}
+                      width={100}
+                      height={100}
+                      src={croppedImage}
+                    />
+                  )}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Modal.Content>
+          <Modal.Actions>
+            {croppedImage && (
+              <Button color="green">
+                <Icon name="save" /> Change Avatar
+              </Button>
+            )}
+            <Button color="green" onClick={this.handleCropImage}>
+              <Icon name="image" /> Preview
+            </Button>
+            <Button color="red" onClick={this.closeModal}>
+              <Icon name="remove" /> Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </Grid>
     )
   }
