070b263159c95e4a24d638cfebbd449fbf3b6cd5
Scott Schottler <scottschottler@gmail.com>
2019-01-08 16:14:22 -0500

    add favorite channels to starred section

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -18,7 +18,7 @@ export class SidePanel extends Component {
         vertical
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}>
         <UserPanel currentUser={currentUser} />
-        <Starred />
+        <Starred currentUser={currentUser} />
         <Channels currentUser={currentUser} />
         <DirectMessages currentUser={currentUser} />
       </Menu>
