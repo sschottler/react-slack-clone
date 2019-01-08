7e71c9c0fe9538a3be67424e2930f71e1304adfd
Scott Schottler <scottschottler@gmail.com>
2019-01-08 16:55:36 -0500

    add color pickers

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
-import { Sidebar, Menu, Divider, Button } from 'semantic-ui-react'
+import {
+  Sidebar,
+  Menu,
+  Divider,
+  Button,
+  Modal,
+  Icon,
+  Label
+} from 'semantic-ui-react'
+import { SliderPicker } from 'react-color'
 
 export class ColorPanel extends Component {
+  state = {
+    modal: false
+  }
+
+  openModal = () => this.setState({ modal: true })
+  closeModal = () => this.setState({ modal: false })
+
   render() {
+    const { modal } = this.state
+
     return (
       <Sidebar
         as={Menu}
@@ -12,7 +30,25 @@ export class ColorPanel extends Component {
         visible
         width="very thin">
         <Divider />
-        <Button icon="add" size="small" color="blue" />
+        <Button onClick={this.openModal} icon="add" size="small" color="blue" />
+
+        <Modal open={modal} onClose={this.closeModal}>
+          <Modal.Header>Choose App Colors</Modal.Header>
+          <Modal.Content>
+            <Label content="Primary Color" />
+            <SliderPicker />
+            <Label content="Secondary Color" />
+            <SliderPicker />
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green">
+              <Icon name="checkmark" /> Save Color
+            </Button>
+            <Button color="red" onClick={this.closeModal}>
+              <Icon name="remove" /> Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </Sidebar>
     )
   }
