c80afb8aded651cb76c0eac3ef07f19ea9ee12c1
Scott Schottler <scottschottler@gmail.com>
2019-01-08 16:28:17 -0500

    added channeldetails to metapanel

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { Segment, Accordion, Header, Icon } from 'semantic-ui-react'
+import { Segment, Image, Accordion, Header, Icon } from 'semantic-ui-react'
 
 export class MetaPanel extends Component {
   state = {
+    channel: this.props.currentChannel,
     privateChannel: this.props.isPrivateChannel,
     activeIndex: 0
   }
@@ -15,14 +16,14 @@ export class MetaPanel extends Component {
   }
 
   render() {
-    const { activeIndex, privateChannel } = this.state
+    const { activeIndex, channel, privateChannel } = this.state
 
     if (privateChannel) return null
 
     return (
-      <Segment>
+      <Segment loading={!channel}>
         <Header as="h3" attached="top">
-          About # Channel
+          About # {channel && channel.name}
         </Header>
         <Accordion styled attached="true">
           <Accordion.Title
@@ -34,7 +35,7 @@ export class MetaPanel extends Component {
             Channel Details
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 0}>
-            details
+            {channel && channel.details}
           </Accordion.Content>
 
           <Accordion.Title
@@ -58,7 +59,10 @@ export class MetaPanel extends Component {
             Created By
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 2}>
-            creator
+            <Header as="h3">
+              <Image circular src={channel && channel.createdBy.avatar} />
+              {channel && channel.createdBy.name}
+            </Header>
           </Accordion.Content>
         </Accordion>
       </Segment>
